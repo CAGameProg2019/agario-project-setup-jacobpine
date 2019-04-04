5aaafdc7f3ca869d3bfa36bb56ad97be5b04899d
Jake Pine <pinejh95@gmail.com>
2019-04-04 09:21:01 -0400

    vector class

--- vector.js
+++ vector.js
@@ -1,7 +1,34 @@
 class Vector {
     
-    constructor() {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 
+    addVector(vec) {
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+
+    // Functions to write for HW
+    subVector(vec) {
+        this.x -= vec.x;
+        this.y -= vec.y;
+        return this;
+    }
+
+    scale(s) {
+        this.x *= s;
+        this.y *= s;
+        return this;
+    }
+
+    toString() {
+        return '<'+this.x+', '+this.y+'>';
+    }
+    print() {
+        console.log(this.toString());
     }
 
 }
