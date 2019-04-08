e85e888c4a82d06256e0b9fe8301905cf1da1c89
Jake Pine <pinejh95@gmail.com>
2019-04-08 10:47:05 -0400

    HW for 4/8

--- main.js
+++ main.js
@@ -4,24 +4,41 @@ let c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let pos = new Vector(60, 60);
-let vel = new Vector(.5, .2);
+let foods = [];
+
+let colors = [
+    'blue',
+    'green',
+    'red',
+    'orange',
+    'purple',
+    'teal'
+];
+
+function randomColor() {
+    let index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
 
 function init() {
-    pos.print();
-    
+
+    for(let i = 0; i < 100; i++) {
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+        let color = randomColor();
+        let food = new Food(x, y, 10, color);
+        foods.push(food);
+    }
+
     update();
 }
 
 function update() {
     c.clearRect(0, 0, canvas.width, canvas.height);
 
-    pos.addVector(vel);
-
-    c.beginPath();
-    c.arc(pos.x, pos.y, 50, 0, Math.PI*2, false);
-    c.closePath();
-    c.stroke();
+    for(let i = 0; i < foods.length; i++) {
+        foods[i].draw(c);
+    }
 
     requestAnimationFrame(update);
 }
