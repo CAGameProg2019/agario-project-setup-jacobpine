c77192a284e092b09d4728f5f2281bc29524d4ea
Jake Pine <pinejh95@gmail.com>
2019-04-11 09:11:55 -0400

    player follows mouse

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ let c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let mpos;
+
+let player;
 let foods = [];
 
 let colors = [
-    'blue',
-    'green',
-    'red',
-    'orange',
-    'purple',
-    'teal'
+    '#FFFF00',
+    '#FF9900',
+    '#FF0000',
+    '#00FF00',
+    '#00FFFF',
+    '#0099FF',
+    '#FF00FF',
+    '#9900FF'
 ];
 
 function randomColor() {
@@ -22,11 +27,15 @@ function randomColor() {
 
 function init() {
 
+    mpos = new Vector(canvas.width/2, canvas.height/2);
+
+    player = new Player(canvas.width/2, canvas.height/2, 25, randomColor());
+
     for(let i = 0; i < 100; i++) {
         let x = Math.random() * canvas.width;
         let y = Math.random() * canvas.height;
         let color = randomColor();
-        let food = new Food(x, y, 10, color);
+        let food = new Food(x, y, 8, color);
         foods.push(food);
     }
 
@@ -36,13 +45,23 @@ function init() {
 function update() {
     c.clearRect(0, 0, canvas.width, canvas.height);
 
+    player.x = mpos.x;
+    player.y = mpos.y;
+
     for(let i = 0; i < foods.length; i++) {
         foods[i].draw(c);
     }
 
+    player.draw(c);
+
     requestAnimationFrame(update);
 }
 
-window.addEventListener('load', function(event) {
+window.addEventListener('load', function() {
     init();
+
+    window.addEventListener('mousemove', function(event) {
+        mpos.x = event.clientX - canvas.offsetLeft;
+        mpos.y = event.clientY - canvas.offsetTop;
+    });
 });
